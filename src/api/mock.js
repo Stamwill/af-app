0278910960c0db40e5159368ec2103a428f8028f
William Stam <williamstam@gmail.com>
2020-05-08 09:52:27 +0100

    [Mock] Added data for gallery

--- src/api/mock.js
+++ src/api/mock.js
@@ -21,3 +21,128 @@ export const navType = {
 	]
 }
 
+export const cardType ={
+	cards: [
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "How to find the perfect trekking trousers",
+			text: "Why are there so many models? The answer is as simple as it is obvious: We’re all different."
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "From the outdoors",
+			text: "So how does a backpack actually come about? We asked William"
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "10 things to do today",
+			text: "why wait just do it now!"
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "The importance of leaving a trace",
+			text: "Protect wildlife, stop fired, leave a trace right now!"
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "The cycle of life",
+			text: "We need to find our own footprints, even bigfoots"
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "Care at home, not outside",
+			text: "Life cycle and party with your own hands, always stay repaired!"
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "Above the trees with cats",
+			text: "Why are there so many cats in trees? Only the cats will ever know."
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "Foxes are crazy",
+			text: "Ever seen them hunt? Just dive into the snow nose first..Ouch"
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "Read about my snowball collection",
+			text: "Why would you want to do that, how bored are you? "
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "Our ocean is blue",
+			text: "Why is the ocean blue and so tasty looking? We can show you."
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "A good tent is the right tent",
+			text: "Don't get caught up in buying fake tents, buy a tent that can handle any activity."
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "Trek with brekk",
+			text: "Go trekking dont stop brekking, eat on the go"
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "When you sweat, pay your debt",
+			text: "We interviewed our sweat and debt expert about things you need to know."
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "How many minutes is enough to make a difference",
+			text: "With enough minutes in your life you can make a difference on a lot of things"
+		},
+		{
+			image: {
+				src: "",
+				alt: ""
+			},
+			title: "How to camp in the winter",
+			text: "Honestly, dress warm bring hot coco and dont wear spandex"
+		},
+	]
+}
+
