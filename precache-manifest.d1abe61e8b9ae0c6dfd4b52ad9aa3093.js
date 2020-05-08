64728e4534d33ff99d73a9f5a36dc2de5e6c85da
William Stam <williamstam@gmail.com>
2020-05-08 09:20:41 +0100

    Updates

---
+++ precache-manifest.d1abe61e8b9ae0c6dfd4b52ad9aa3093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae8013690cc2807c2e5bbb62f526171",
+    "url": "/af-app/index.html"
+  },
+  {
+    "revision": "a517a185c0b397809eb2",
+    "url": "/af-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "279a729ec930014381e4",
+    "url": "/af-app/static/js/2.75030a66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/af-app/static/js/2.75030a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a517a185c0b397809eb2",
+    "url": "/af-app/static/js/main.41eedfc8.chunk.js"
+  },
+  {
+    "revision": "39ef6995939eb23e6cdd",
+    "url": "/af-app/static/js/runtime-main.a89bcc58.js"
+  }
+]);
