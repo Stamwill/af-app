e61973574dbb75da3f5cdeda52650fb7ae0626f9
William Stam <williamstam@gmail.com>
2020-05-08 09:37:10 +0100

    [mock] Added data for nav

---
+++ src/api/mock.js
@@ -0,0 +1,23 @@
+export const navType = {
+	link: [
+		{
+			link: {
+				title: "Adventures",
+				href: "#"
+			},
+			link: {
+				title: "Guides",
+				href: "#"
+			},
+			link: {
+				title: "Sustainability",
+				href: "#"
+			},
+			link: {
+				title: "Products",
+				href: "#"
+			},
+		}
+	]
+}
+
