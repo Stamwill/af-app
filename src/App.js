bf5ec1d816b42c279dc0b19868b99fe22ca6631e
William Stam <williamstam@gmail.com>
2020-05-09 11:50:28 +0100

    [App] changed props to Gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import * as api from 'api/mock'
 import Banner from 'components/Banner'
+import Gallery from 'components/Gallery'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Banner menu={api.menuPrimary} />
+      <Gallery {...api.cardPage} />
     </div>
   )
 }
