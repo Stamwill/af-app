972fd2a8735009bfc75212fe9397125176ab8ed4
William Stam <williamstam@gmail.com>
2020-05-11 14:46:46 +0100

    [PageButtons] added forwardRef and changed propTypes

--- src/components/PageButtons/PageButtons.js
+++ src/components/PageButtons/PageButtons.js
@@ -2,8 +2,50 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import classes from './PageButtons.module.css'
 
+const buttons = [
+  {
+    label: '1',
+    url: '#',
+  },
+  {
+    label: '2',
+    url: '#',
+  },
+  {
+    label: '3',
+    url: '#',
+  },
+  {
+    label: '4',
+    url: '#',
+  },
+  {
+    label: '5',
+    url: '#',
+  },
+  {
+    label: '6',
+    url: '#',
+  },
+  {
+    label: '7',
+    url: '#',
+  },
+  {
+    label: '8',
+    url: '#',
+  },
+  {
+    label: '9',
+    url: '#',
+  },
+  {
+    label: '10',
+    url: '#',
+  },
+]
+
 function PageButtons(props) {
-  const { buttons } = props
   return (
     <div className={classes.root}>
       {buttons.map((button, idx) => (
