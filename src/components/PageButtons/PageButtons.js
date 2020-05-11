b8b8c4da177ffc31b6c19676a8d90e9d9b469169
William Stam <williamstam@gmail.com>
2020-05-11 14:49:47 +0100

    [PageButtons] changed propTypes

--- src/components/PageButtons/PageButtons.js
+++ src/components/PageButtons/PageButtons.js
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'clsx'
+// import { linkType } from 'utils'
 import classes from './PageButtons.module.css'
 
 const buttons = [
@@ -45,25 +47,22 @@ const buttons = [
   },
 ]
 
-function PageButtons(props) {
+const PageButtons = React.forwardRef(function PageButtons(props, ref) {
+  const { className, ...other } = props
   return (
-    <div className={classes.root}>
+    <div className={classnames(classes.root, className)} ref={ref} {...other}>
       {buttons.map((button, idx) => (
-        <button className={classes.button} type="button" href={button.url} key={idx}>
+        <button key={idx} className={classes.button} type="button" href={button.url}>
           {button.label}
         </button>
       ))}
     </div>
   )
-}
+})
 
 PageButtons.propTypes = {
-  buttons: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string,
-      url: PropTypes.string,
-    }),
-  ),
+  // buttons: PropTypes.arrayOf(linkType).isRequired,
+  className: PropTypes.string,
 }
 
 export default PageButtons
