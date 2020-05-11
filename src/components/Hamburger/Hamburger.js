be5fb66242f1489f1112726bc835a26438e383a5
William Stam <williamstam@gmail.com>
2020-05-11 15:26:30 +0100

    [Hamburger] Changed comp to forwardRef

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,13 +1,21 @@
 import * as React from 'react'
+import classnames from 'clsx'
+import PropTypes from 'prop-types'
 import logo from './burgerLogo.png'
 import classes from './Hamburger.module.css'
 
-function Hamburger() {
+const Hamburger = React.forwardRef(function Hamburger(props, ref) {
+  const { className, ...other } = props
+
   return (
-    <div>
+    <div className={classnames(classes.root, className)} ref={ref} {...other}>
       <img className={classes.logo} src={logo} alt="" />
     </div>
   )
+})
+
+Hamburger.propTypes = {
+  className: PropTypes.string,
 }
 
 export default Hamburger
