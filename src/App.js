da22f7ed5a78561399dce415766c68d284b5f5fe
William Stam <williamstam@gmail.com>
2020-05-11 12:20:18 +0100

    [App] imported PageButtons and passed mock data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import * as api from 'api/mock'
+import Navbar from 'components/Navbar'
 import Banner from 'components/Banner'
 import Gallery from 'components/Gallery'
+import PageButtons from 'components/PageButtons'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Navbar menu={api.menuPrimary} />
       <Banner menu={api.menuPrimary} />
+      <PageButtons buttons={api.galleryNumber} />
       <Gallery {...api.cardPage} />
     </div>
   )
