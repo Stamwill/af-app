b35c03e204a9c1caaef2e15f96e491e9b1fcafb0
William Stam <williamstam@gmail.com>
2020-05-11 14:27:43 +0100

    [utils] add articleType to common exports

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
-export { default as mediaType } from './mediaType'
+export { default as articleType } from './articleType'
 export { default as cardType } from './cardType'
-export { default as menuLinkType } from './menuLinkType'
 export { default as linkType } from './linkType'
+export { default as mediaType } from './mediaType'
+export { default as menuLinkType } from './menuLinkType'
