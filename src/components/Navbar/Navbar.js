b4b62bfc91be88027cf299afa8ec8d52d18e168a
William Stam <williamstam@gmail.com>
2020-05-11 15:27:01 +0100

    [Navbar] Changed comp to forwardRef

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,15 @@
 import * as React from 'react'
+import classnames from 'clsx'
+import PropTypes from 'prop-types'
 import Search from 'components/Search'
 import Hamburger from 'components/Hamburger'
 import classes from './Navbar.module.css'
 
-function Navbar(props) {
+const Navbar = React.forwardRef(function Navbar(props, ref) {
+  const { className, ...other } = props
+
   return (
-    <div className={classes.root}>
+    <div className={classnames(classes.root, className)} ref={ref} {...other}>
       <div>
         <img className={classes.image} src="//source.unsplash.com/40x50?bear" alt="" />
       </div>
@@ -21,6 +25,10 @@ function Navbar(props) {
       </div>
     </div>
   )
+})
+
+Navbar.propTypes = {
+  className: PropTypes.string,
 }
 
 export default Navbar
