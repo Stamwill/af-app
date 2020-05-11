1a46b2dae5568a89294663dc7472d433ea8b4986
William Stam <williamstam@gmail.com>
2020-05-11 15:07:07 +0100

    [Banner] fixed propTypes and forwardRef

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,19 +1,25 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'clsx'
 import Navigation from 'components/Navigation'
 import classes from './Banner.module.css'
 
-function Banner(props) {
-  const { menu } = props
+const Banner = React.forwardRef(function Banner(props, ref) {
+  const { menu, className, ...other } = props
 
   return (
-    <div className={classes.root}>
+    <div className={classnames(classes.root, className)} ref={ref} {...other}>
       <h1 className={classes.heading}>Martin tittar, se upptagna ut</h1>
 
       <img className={classes.background} src="//source.unsplash.com/1080x715?hiking" alt="" />
       <Navigation menu={menu} />
-      <div></div>
     </div>
   )
+})
+
+Banner.propTypes = {
+  menu: PropTypes.string,
+  className: PropTypes.string,
 }
 
 export default Banner
