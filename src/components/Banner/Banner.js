15e17618f5ad8209f5d6c71d8c2c297adec617a7
William Stam <williamstam@gmail.com>
2020-05-11 15:25:51 +0100

    [Banner] Changed comp to forward ref and props

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,20 +5,20 @@ import Navigation from 'components/Navigation'
 import classes from './Banner.module.css'
 
 const Banner = React.forwardRef(function Banner(props, ref) {
-  const { menu, className, ...other } = props
+  const { menus, className, ...other } = props
 
   return (
     <div className={classnames(classes.root, className)} ref={ref} {...other}>
       <h1 className={classes.heading}>Martin tittar, se upptagna ut</h1>
 
       <img className={classes.background} src="//source.unsplash.com/1080x715?hiking" alt="" />
-      <Navigation menu={menu} />
+      <Navigation menus={menus} />
     </div>
   )
 })
 
 Banner.propTypes = {
-  menu: PropTypes.string,
+  menus: PropTypes.string,
   className: PropTypes.string,
 }
 
