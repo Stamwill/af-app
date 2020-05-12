4015a2849f732a00dc1d17c0168887147710ac78
William Stam <williamstam@gmail.com>
2020-05-12 12:33:18 +0100

    [App] Added Footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import * as api from 'api/mock'
 import Home from 'pages/Home'
 import Navbar from 'components/Navbar'
 import Banner from 'components/Banner'
+import Footer from 'components/Footer'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Navbar menu={api.menuPrimary} />
+      <Navbar menus={api.menuPrimary} />
       <Banner menus={api.menuPrimary} />
       <Home {...api.startPage} />
+      <Footer />
     </div>
   )
 }
