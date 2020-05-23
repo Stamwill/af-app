183d7d39a7384d896858bf5f5f2fecc6efb5ac6e
William Stam <williamstam@gmail.com>
2020-05-23 14:22:43 +0100

    [Search] passed props and fixed styling for search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,22 +5,21 @@ import classes from './Search.module.css'
 import logo from './searchLogo.png'
 
 const Search = React.forwardRef(function Search(props, ref) {
-  const { className, ...other } = props
+  const { toggleSearch, className, ...other } = props
 
   return (
     <div className={classnames(classes.root, className)} ref={ref} {...other}>
-      <a className={classes.search} href="/">
-        <div>
-          <img className={classes.logo} src={logo} alt="" />
-          <span className={classes.span}>Search</span>
-        </div>
-      </a>
+      <div className={classes.search} onClick={toggleSearch}>
+        <img className={classes.logo} src={logo} alt="" />
+        <span className={classes.span}>Search</span>
+      </div>
     </div>
   )
 })
 
 Search.propTypes = {
   className: PropTypes.string,
+  toggleSearch: PropTypes.func,
 }
 
 export default Search
