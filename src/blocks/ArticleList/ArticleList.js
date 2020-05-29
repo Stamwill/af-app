61f86f9e47c2c4e26e4c8a054b1f279dcc1f18f1
William Stam <williamstam@gmail.com>
2020-05-29 17:32:21 +0100

    [ArticleList] Import articles, change div to Section

--- src/blocks/ArticleList/ArticleList.js
+++ src/blocks/ArticleList/ArticleList.js
@@ -1,20 +1,30 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'clsx'
+import { articleType } from 'utils'
+import ArticleItem from 'containers/ArticleItem'
+import Section from 'components/Section'
 import ArticleListPagination from './partials/ArticleListPagination'
 import classes from './ArticleList.module.css'
 
 const ArticleList = React.forwardRef(function ArticleList(props, ref) {
-  const { className, ...other } = props
+  const { className, articles, ...other } = props
 
   return (
-    <div className={classnames(classes.root, className)} ref={ref} {...other}>
-      <ArticleListPagination />
-    </div>
+    <Section className={classnames(classes.root, className)} ref={ref} {...other}>
+      <ArticleListPagination className={classes.pagination} />
+
+      <div className={classes.list}>
+        {articles.map((article, idx) => (
+          <ArticleItem key={idx} article={article} />
+        ))}
+      </div>
+    </Section>
   )
 })
 
 ArticleList.propTypes = {
+  articles: PropTypes.arrayOf(articleType).isRequired,
   className: PropTypes.string,
 }
 
