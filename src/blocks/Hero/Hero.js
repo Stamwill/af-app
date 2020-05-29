f8cb704809fa256387ee7ada33e9c18ef2539e7c
William Stam <williamstam@gmail.com>
2020-05-29 17:35:26 +0100

    [Hero] Import Section, change div to Section

--- src/blocks/Hero/Hero.js
+++ src/blocks/Hero/Hero.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'clsx'
 import { menuLinkType } from 'utils'
+import Section from 'components/Section'
 import HeroNav from './partials/HeroNav'
 import classes from './Hero.module.css'
 
@@ -9,11 +10,13 @@ const Hero = React.forwardRef(function Hero(props, ref) {
   const { menu, className, ...other } = props
 
   return (
-    <div className={classnames(classes.root, className)} ref={ref} {...other}>
+    <Section className={classnames(classes.root, className)} ref={ref} {...other}>
       <h1 className={classes.heading}>Power of the Bear</h1>
+
       <img className={classes.background} src="//source.unsplash.com/1080x715?hiking" alt="" />
+
       <HeroNav primary={menu} />
-    </div>
+    </Section>
   )
 })
 
