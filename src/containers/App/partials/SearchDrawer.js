10a89555210a2be8a180265b88cf975367b22065
William Stam <williamstam@gmail.com>
2020-06-03 10:36:28 +0100

    [SearchDrawer] Add useEffect with focus on input, style SearchIsOpen and Root

--- src/containers/App/partials/SearchDrawer.js
+++ src/containers/App/partials/SearchDrawer.js
@@ -3,14 +3,22 @@ import PropTypes from 'prop-types'
 import classnames from 'clsx'
 import classes from './SearchDrawer.module.css'
 
-const toggleScroll = (state = 'auto') => {
-  const body = document.body.style
-
-  body.overflow = state
-}
 const SearchDrawer = React.forwardRef(function SearchDrawer(props, ref) {
   const { open, className, ...other } = props
 
+  const inputRef = React.useRef(null)
+
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  })
+
+  const toggleScroll = (state = 'auto') => {
+    const body = document.body.style
+
+    body.overflow = state
+  }
   if (open) {
     toggleScroll('hidden')
   } else {
@@ -25,8 +33,12 @@ const SearchDrawer = React.forwardRef(function SearchDrawer(props, ref) {
     >
       <div className={classes.content}>
         <header className={classes.heading} />
-
-        <input className={classes.input} placeholder="Type to search..." />
+        <input
+          className={classes.input}
+          type="text"
+          placeholder="Type to search..."
+          ref={inputRef}
+        />
       </div>
     </div>
   )
